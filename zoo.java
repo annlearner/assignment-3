051b9b24a709d6a1c1a0126ccd094ff568115c1d
AnithaKanna <35582681+AnithaKanna@users.noreply.github.com>
2018-02-08 23:01:37 -0800

    Add files via upload

---
+++ zoo.java
@@ -0,0 +1,13 @@
+public class zoo {
+    public static void main(String args[]){
+       bird crow=new bird(2,"male",20);
+       crow.fly();
+       animal horse=new animal(9,"m",200);
+       horse.eat();
+       chicken chick1 = new chicken(2,"m",5);
+        chick1.fly();
+        sparrow sparrow1=new sparrow(1,"f",3);
+        sparrow1.fly();
+
+    }
+}
