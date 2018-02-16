e3aeb8d2a9da59841bff35767fe1d8fcae409bd1
annlearner <31709240+annlearner@users.noreply.github.com>
2018-02-16 12:29:55 -0800

    Update salary.java

--- salary.java
+++ salary.java
@@ -1,3 +1,6 @@
+// please pay attention to that write the main function and the method function in the same class, and print the result in the main function,
+// use "return" to get the value. you should pay attention to this, otherwise scores will be deducted next time.
+//scores : 10 /10
 public class salary {
     double NoofHours;
     double empsalary;
